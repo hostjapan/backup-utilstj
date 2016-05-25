350ba12701bcbadfe30a5536f362dad842142df1
Sergio Rubio <rubiojr@frameos.org>
2016-05-25 16:25:39 +0200

    Benchmark the restore process

---
+++ share/github-backup-utils/bm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# bm.sh: benchmarking Bash code blocks
+#
+# Example:
+#   bm_start "wget request"
+#   wget --quiet https://www.google.com
+#   bm_end "wget request"
+#
+# Sample output:
+#   $ bash test.sh
+#   'wget request' took: 2s
+
+bm_desc_to_varname(){
+ echo "__bm$(echo $@ | tr -cd '[[:alnum:]]')"
+}
+
+bm_start()
+{
+  eval "$(bm_desc_to_varname $@)_start=$(date +%s)"
+}
+
+bm_end() {
+  local tend=$(date +%s)
+  local tstart=$(eval "echo \$$(bm_desc_to_varname $@)_start")
+
+  echo "'$1' took: $(($tend - $tstart))s" >> $GHE_SNAPSHOT_DIR/.benchmark.tmp
+}
